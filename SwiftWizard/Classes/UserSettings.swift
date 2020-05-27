0571a73ecab2781129129d0cc4456e885be3a04d
Joseph Bellahcen <tornupnegatives@gmail.com>
2020-05-27 17:44:29 -0500

    Re-organization of properties

--- SwiftWizard/Classes/UserSettings.swift
+++ SwiftWizard/Classes/UserSettings.swift
@@ -1,15 +1,10 @@
-//
-//  UserSettings.swift
-//  SwiftWizard
-//
-//  Created by Joseph Bellahcen on 5/26/20.
-//  Copyright © 2020 Joseph Bellahcen. All rights reserved.
-//
+// The UserSettings singleton provides some global defaults for controlling SwiftWizard
 
 import Foundation
 
 struct UserSettings{
-    //let sharedInstance:             UserSettings = UserSettings()
+    static var sharedInstance:      UserSettings = UserSettings()
+    
     var preEmphasisAlpha:           Double
     var sampleRate:                 Double
     var exportSampleRate:           Double
@@ -25,9 +20,10 @@ struct UserSettings{
     var lowPassCutoff:              Double
     var highPassCutoff:             Double
     var speed:                      Double
-    var windowWidth:                Double!
     var unvoicedMultiplier:         Double
     var gain:                       Double
+    
+    var windowWidth:                Double!
     var startSample:                Double!
     var endSample:                  Double!
 
@@ -39,32 +35,8 @@ struct UserSettings{
     var skipLeadingSilence:         Bool!
     var includeHexPrefix:           Bool!
     var includeExplicitStopFrame:   Bool!
-    
-    init(preEmphasisAlpha: Double?, sampleRate: Double?, frameRate: Double?, exportSampleRate: Double?, maxPitchInHZ: Double?,
-         minPitchInHZ: Double?, subMultipleThreshold: Double?, pitchValue: Double?, pitchOffset: Double?, unvoicedThreshold: Double?,
-         rmsLimit: Double?, unvoicedRMSLimit: Double?, lowPassCutoff: Double?, highPassCutoff: Double?, speed: Double?,
-         unvoicedMultiplier: Double?, gain: Double?, windowWidth: Double?){
-        self.preEmphasisAlpha       = preEmphasisAlpha      ?? -0.93750
-        self.sampleRate             = sampleRate            ?? 8000
-        self.frameRate              = frameRate             ?? 25.0
-        self.exportSampleRate       = exportSampleRate      ?? 48000
-        self.maxPitchInHZ           = maxPitchInHZ          ?? 500
-        self.minPitchInHZ           = minPitchInHZ          ?? 50
-        self.subMultipleThreshold   = subMultipleThreshold  ?? 0.9
-        self.pitchValue             = pitchValue            ?? 0
-        self.pitchOffset            = pitchOffset           ?? 0
-        self.unvoicedThreshold      = unvoicedThreshold     ?? 0.3
-        self.rmsLimit               = rmsLimit              ?? 14
-        self.unvoicedRMSLimit       = unvoicedRMSLimit      ?? 14
-        self.lowPassCutoff          = lowPassCutoff         ?? 48000
-        self.highPassCutoff         = highPassCutoff        ?? 0
-        self.speed                  = speed                 ?? 1.0
-        self.unvoicedMultiplier     = unvoicedMultiplier    ?? 0.5
-        self.gain                   = gain                  ?? 1.0
-        self.windowWidth            = windowWidth           ?? 2
-    }
-    
-    init(){
+
+    private init(){
         self.preEmphasisAlpha       = -0.93750
         self.sampleRate             = 8000
         self.frameRate              = 25.0
