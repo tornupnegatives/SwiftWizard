b9d8d36f967a510a755ed83f0ff74bb75497c1ef
Joseph Bellahcen <tornupnegatives@gmail.com>
2020-05-27 11:22:44 -0500

    Documentation and error-correction

--- SwiftWizard/Classes/Autocorrelator.swift
+++ SwiftWizard/Classes/Autocorrelator.swift
@@ -1,27 +1,25 @@
-//
-//  Autocorrelator.swift
-//  SwiftWizard
-//
-//  Created by Joseph Bellahcen on 5/26/20.
-//  Copyright © 2020 Joseph Bellahcen. All rights reserved.
-//
+// The Autocorrelator (aka Serial Correlator) processes the audio signal using a delayed copy of itself
+// Digital audio input can be represented as a discrete-time signal (finite energy), modeled as a square-summable function
+// The resulting coefficients describe the relationship between any two consecutive points
 
 import Foundation
 
 final class Autocorrelator {
-    static func getCoefficientsFor(coefficients: [Double], buffer: Buffer) -> [Double] {
-        var newCoefficients: [Double] = [Double]()
+    // Returns autocorrelated coefficients from a given buffer
+    static func getCoefficients(coefficients: [Double], buffer: Buffer) -> [Double] {
+        var newCoefficients: [Double] = Array(repeating: 0, count: 11)
         for i in 0...10 {
-            newCoefficients.append(aForLag(lag: UInt(i), buffer: buffer))
+            newCoefficients[i] = lagSum(lag: UInt(i), buffer: buffer)
         }
         return newCoefficients
     }
     
-    static func sumOfSquaresFor(buffer: Buffer) -> Double {
-        return aForLag(lag: 0, buffer: buffer)
+    static func sumOfSquares(buffer: Buffer) -> Double {
+        return lagSum(lag: 0, buffer: buffer)
     }
     
-    private static func aForLag(lag: UInt, buffer: Buffer) -> Double {
+    // Given lag in units time, lagSum returns a "sum of squares" for consecutive lag-separated values
+    private static func lagSum(lag: UInt, buffer: Buffer) -> Double {
         let samples: UInt   = buffer.size - lag
         var sum:     Double = 0.0
         
@@ -31,16 +29,17 @@ final class Autocorrelator {
         return sum
     }
     
-    static func getNormalizedCoefficientsFor(coefficients: [Double], buffer: Buffer, minPeriod: UInt, maxPeriod: UInt) -> [Double]{
-        var newNormalizedCoefficients: [Double] = [Double]()
+    // Autocorrelates coefficients while also normalizing to given min/max period values
+    static func normalizedCoefficients(coefficients: [Double], buffer: Buffer, minPeriod: UInt, maxPeriod: UInt) -> [Double]{
+        var newNormalizedCoefficients: [Double] = coefficients
         for lag in 0...maxPeriod {
             if lag < minPeriod {
-                newNormalizedCoefficients.append(0.0)
-                continue
+                newNormalizedCoefficients[Int(lag)] = 0.0
             }
             
             var sumOfSquaresBegin:  Double  = 0.0
             var sumOfSquaresEnd:    Double  = 0.0
+            
             var sum:                Double  = 0.0
             let samples:            UInt    = buffer.size - lag
             
@@ -54,4 +53,3 @@ final class Autocorrelator {
         return newNormalizedCoefficients
     }
 }
-
