60200ca388bed58cb1250910b32e1dc0a77e5d99
Joseph Bellahcen <tornupnegatives@gmail.com>
2020-06-05 20:21:45 -0500

    Re-organized

--- SwiftWizard/Classes/Buffer.swift
+++ SwiftWizard/Classes/Buffer.swift
@@ -6,11 +6,7 @@ final class Buffer {
     var size:       Int
     var sampleRate: Int
     var samples:    [Double]!
-    
-    // Included in original but appear unused
-    // var start:      UInt
-    // var end:        UInt
-    
+
     // Instantiate an empty buffer
     init(size: Int, sampleRate: Int) {
         self.size       = size
@@ -23,17 +19,7 @@ final class Buffer {
         self.size       = size
         self.sampleRate = sampleRate
     }
-    
-    // Included in original but appears unused
-    /*
-    init(samples: [Double], size: UInt, sampleRate: UInt, start: UInt, end: UInt) {
-        self.samples    = samples
-        self.size       = size
-        self.sampleRate = sampleRate
-        self.start      = start
-        self.end        = end
-    } */
-    
+
     func energy() -> Double {
         return Autocorrelator.sumOfSquares(buffer: self)
     }
