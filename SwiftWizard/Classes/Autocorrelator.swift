f74903a06b5e788d20fcc5d8797dbb4f0acfab1a
Joseph Bellahcen <tornupnegatives@gmail.com>
2020-06-05 20:19:42 -0500

    Re-organized

--- SwiftWizard/Classes/Autocorrelator.swift
+++ SwiftWizard/Classes/Autocorrelator.swift
@@ -18,17 +18,6 @@ final class Autocorrelator {
         return lagSum(lag: 0, buffer: buffer)
     }
     
-    // Given lag in units time, lagSum returns a "sum of squares" for consecutive lag-separated values
-    private static func lagSum(lag: Int, buffer: Buffer) -> Double {
-        let samples: Int   = buffer.size - lag
-        var sum:     Double = 0.0
-        
-        for i in 0..<samples {
-            sum += buffer.samples[Int(i)] * buffer.samples[i + lag]
-        }
-        return sum
-    }
-    
     // Autocorrelates coefficients while also normalizing to given min/max period values
     static func normalizedCoefficients(coefficients: [Double], buffer: Buffer, minPeriod: Int, maxPeriod: Int) -> [Double]{
         var newNormalizedCoefficients: [Double] = coefficients
@@ -51,5 +40,16 @@ final class Autocorrelator {
             newNormalizedCoefficients[lag] = sum / sqrt(sumOfSquaresBegin * sumOfSquaresEnd)
         }
         return newNormalizedCoefficients
+    }
+    
+    // Given lag in units time, lagSum returns a "sum of squares" for consecutive lag-separated values
+    private static func lagSum(lag: Int, buffer: Buffer) -> Double {
+        let samples: Int   = buffer.size - lag
+        var sum:     Double = 0.0
+        
+        for i in 0..<samples {
+            sum += buffer.samples[Int(i)] * buffer.samples[i + lag]
+        }
+        return sum
     }
 }
