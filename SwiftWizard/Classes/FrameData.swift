60a574aa5c0eed68d78f55b1f00fa20c9a096ea8
Joseph Bellahcen <tornupnegatives@gmail.com>
2020-06-15 22:01:20 -0500

    Commit FrameData

--- SwiftWizard/Classes/FrameData.swift
+++ SwiftWizard/Classes/FrameData.swift
@@ -1,26 +1,33 @@
-// The FrameData class takes kParameters and their values and prepares them for processing
+// The FrameData class takes creates a dictionary with pairs of KParameters and their values
+// The number of parameters in a frame will depend on what sound that frame describes
 
 import Foundation
 
 final class FrameData {
-    var shouldSkip:                           Bool = false
-    var reflector:                            Reflector
+    var reflector: Reflector
     
-    private var pitch:                        Double
-    private var repeats:                      Bool
-    private var isDecodeFrame:                Bool = true
-    private var isStopFrame:                  Bool = false
-    private var parameters:                   [String: Double]?
-    private var translatedParameters:         [String: Double]?
+    private var parameters: [String: Double]?
+    private var translatedParameters: [String: Double]?
     
+    private var pitch: Double
+    private var repeats: Bool
+    private var isStopFrame:   Bool
+    private var isForDeocding: Bool
     
+    init(reflector: Reflector, pitch: Double, repeats: Bool) {
+        self.reflector      = reflector
+        self.pitch          = pitch
+        self.repeats        = repeats
+        self.isStopFrame    = false
+        self.isForDeocding  = false
+    }
     
     static func stopFrame() -> FrameData {
         let reflector: Reflector = Reflector()
-        reflector.setRMS(rms: Double(CodingTable.rms[kStopFrameIndex]))
+        reflector.rms = CodingTable.rms[kStopFrameIndex]
         
         let frameData: FrameData = FrameData(reflector: reflector, pitch: 0, repeats: false)
-        frameData.isStopFrame = true
+        frameData.isStopFrame    = true
         
         return frameData
     }
@@ -28,49 +35,86 @@ final class FrameData {
     static func frameForDecoding() -> FrameData {
         let reflector: Reflector = Reflector()
         let frameData: FrameData = FrameData(reflector: reflector, pitch: 0, repeats: false)
-        frameData.isDecodeFrame = true
+        
+        frameData.isForDeocding  = true
         
         return frameData
     }
     
-    init(reflector: Reflector, pitch: Double, repeats: Bool) {
-        self.reflector = reflector
-        self.pitch     = pitch
-        self.repeats   = repeats
-    }
-    
     func getParameters() -> [String: Double] {
-        if parameters != nil {
-            return parameters!
+        if parameters == nil {
+            parameters = parametersWithTranslate(translate: false)
+        }
+        return parameters!
+    }
+    
+    func setParameter(parameter: String, value: Double) {
+        parameters = nil
+        translatedParameters = nil
+        
+        if parameter == kParameterGain {
+            let index: Int    = Int(value)
+            let rms:   Double = CodingTable.rms[index]
+            reflector.rms     = rms
+            
+        } else if parameter == kParameterRepeat {
+            repeats = (value != 0)
+            
+        } else if parameter == kParameterPitch {
+            let index: Int    = Int(value)
+            let pitch: Double = CodingTable.pitch[index]
+            self.pitch        = Double(pitch)
         } else {
-            return parametersWithTranslate(translate: false)
+            let startSubString: String.Index = parameter.index(parameter.startIndex, offsetBy: 1)
+            let bin:   Int    = Int(parameter[startSubString...]) ?? 0
+            let index: Int    = Int(value)
+            let k:     Double = try! CodingTable.kBinFor(k: bin)[index] // TODO: Handle exception
+            
+            reflector.ks[bin] = k
         }
     }
     
-    private func getTranslatedParameters() -> [String: Double]? {
-        if translatedParameters != nil {
-            return translatedParameters
+    func setParameter(parameter: String, translatedValue: Double) {
+        parameters = nil
+        translatedParameters = nil
+        
+        if parameter == kParameterGain {
+            reflector.rms = translatedValue
+        } else if parameter == kParameterRepeat {
+            repeats = (translatedValue != 0)
+        } else if parameter == kParameterPitch {
+            pitch = translatedValue
         } else {
-            return parametersWithTranslate(translate: false)
+            let startSubString: String.Index = parameter.index(parameter.startIndex, offsetBy: 1)
+            let bin:   Int    = Int(parameter[startSubString...]) ?? 0
+            
+            reflector.ks[bin] = translatedValue
         }
+        
+    }
+    
+    private func getTranslatedParameters() -> [String: Double] {
+        if translatedParameters == nil {
+            translatedParameters = parametersWithTranslate(translate: true)
+        }
+        return translatedParameters!
     }
     
     private func parametersWithTranslate(translate: Bool) -> [String: Double] {
-        var parameters: [String: Double] = [String: Double]()
-        
+        var parameters: [String: Double] = [String: Double]() // Set to hold kParameterKeys entries
         parameters[kParameterGain] = parameterizedValueForRMS(rms: reflector.rms!, translate: translate)
-        if Double(parameters[kParameterGain]!) > 0.0 {
+        
+        if parameters[kParameterGain]! > 0.0 {
             parameters[kParameterRepeat] = parameterizedValueForRepeat(repeats: repeats)
             parameters[kParameterPitch]  = parameterizedValueForPitch(pitch: pitch, translate: translate)
             
             if parameters[kParameterRepeat] == 0 {
-                if let ks: [String: Double] = kParametersFrom(from: 1, to: 4, translate: translate) {
-                    parameters.merge(ks, uniquingKeysWith: { $1})
-                }
-                    
-                if parameters[kParameterPitch] != 0 && (isDecodeFrame || reflector.isVoiced) {
-                    if let ks =  kParametersFrom(from: 5, to: 10, translate: translate) {
-                        parameters.merge(ks, uniquingKeysWith: { $1})
+                if var ks: [String: Double] = kParametersFrom(from: 1, to: 4, translate: translate) {
+                    parameters.merge(ks){(current, _) in current}
+                
+                    if parameters[kParameterPitch] != 0 && (isForDeocding || reflector.isVoiced) {
+                        ks = kParametersFrom(from: 5, to: 10, translate: translate)!
+                        parameters.merge(ks){(current, _) in current}
                     }
                 }
             }
@@ -78,48 +122,8 @@ final class FrameData {
         return parameters
     }
     
-    func setParameter(parameter: String, value: Int) throws {
-        parameters = nil
-        translatedParameters = nil
-        
-        if parameter == kParameterGain {
-            reflector.setRMS(rms: Double(CodingTable.rms[value]))
-        } else if parameter == kParameterRepeat {
-            repeats = Bool(value != 0)
-        } else if parameter == kParameterPitch {
-            pitch = CodingTable.pitch[value]
-        } else {
-            let binIndex: String.Index = parameter.index(parameter.startIndex, offsetBy: 1) // TODO: Fix this mess
-            let binInt:   Int          = Int(parameter[binIndex...parameter.endIndex]) ?? 0
-            let bin:      String       = String(parameter[binIndex...])
-            let k:        Double       = try CodingTable.kBinFor(k: Int(bin[value].asciiValue ?? 0))[value]
-            reflector.ks[binInt]       = k
-        }
-    }
-    
-    func setParameter(parameter: String, translatedValue: Int) throws {
-        parameters = nil
-        translatedParameters = nil
-        
-        if parameter == kParameterGain {
-            reflector.setRMS(rms: Double(CodingTable.rms[translatedValue]))
-        } else if parameter == kParameterRepeat {
-            repeats = Bool(translatedValue != 0)
-        } else if parameter == kParameterPitch {
-            pitch = CodingTable.pitch[translatedValue]
-        } else {
-            let binIndex: String.Index = parameter.index(parameter.startIndex, offsetBy: 1) // TODO: Fix this mess
-            let binInt:   Int          = Int(parameter[binIndex...parameter.endIndex]) ?? 0
-            let bin:      String       = String(parameter[binIndex...])
-            let k:        Double       = try CodingTable.kBinFor(k: Int(bin[translatedValue].asciiValue ?? 0))[translatedValue]
-            reflector.ks[binInt]     = k
-        }
-    }
-    
     private func parameterizedValueForK(k: Double, bin: Int, translate: Bool) -> Double {
-        let index: Int = ClosestValueFinder.indexFor(actual: k,
-                                                     table: try! CodingTable.kBinFor(k: bin),
-                                                     size:       CodingTable.kSizeFor(k: bin))
+        let index: Int = ClosestValueFinder.indexFor(actual: k, table: try! CodingTable.kBinFor(k: bin), size: CodingTable.kSizeFor(k: bin))
         
         if translate {
             return try! CodingTable.kBinFor(k: bin)[index]
@@ -139,22 +143,31 @@ final class FrameData {
     }
     
     private func parameterizedValueForPitch(pitch: Double, translate: Bool) -> Double {
-        if isDecodeFrame == true {
-            if pitch == 0.0 {return 0}
+        if isForDeocding {
+            if pitch == 0 {
+                return 0
+            }
             
             if UserSettings.sharedInstance.overridePitch {
                 let index: Int = Int(UserSettings.sharedInstance.pitchValue)
                 return CodingTable.pitch[index]
             }
-        } else if reflector.isUnvoiced || pitch == 0.0 {
+        } else if reflector.isUnvoiced || pitch == 0 {
             return 0
         }
         
-        let offset: Int = UserSettings.sharedInstance.overridePitch ? 0 : Int(UserSettings.sharedInstance.pitchOffset)
-        var index: Int = ClosestValueFinder.indexFor(actual: pitch, table: CodingTable.pitch, size: CodingTable.pitchSize()) + offset
+        let offset: Int = Int(UserSettings.sharedInstance.overridePitch ? 0 : UserSettings.sharedInstance.pitchOffset)
+        var index:  Int = ClosestValueFinder.indexFor(actual: pitch, table: CodingTable.pitch, size: CodingTable.pitchSize())
         
-        if index > 63 {index = 63}
-        if index < 0  {index = 0}
+        index += offset
+        
+        if index > 63 {
+            index = 63
+        }
+        
+        if index < 0 {
+            index = 0
+        }
         
         if translate {
             return CodingTable.pitch[index]
@@ -171,21 +184,16 @@ final class FrameData {
         if isStopFrame {
             return nil
         }
-
-        var parameters: [String: Double] = [String: Double]()
         
-        var index: Int = from
-        while index <= to {
-            let key:       String = parameterKeyForK(index: index)
-            let parameter: Double = parameterizedValueForK(k: reflector.ks[index], bin: index, translate: translate)
-            parameters[key]       = parameter
-
-            index += 1
+        var parameters: [String: Double] = [String: Double]()
+        for k in from...to {
+            let key: String = parameterKeyForK(k: k)
+            parameters[key] = parameterizedValueForK(k: reflector.ks[k], bin: k, translate: translate)
         }
         return parameters
     }
     
-    private func parameterKeyForK(index: Int) -> String {
-        return "k\(index)"
+    private func parameterKeyForK(k: Int) -> String {
+        return "k\(k)"
     }
 }
