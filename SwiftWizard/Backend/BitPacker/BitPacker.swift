9bc65a7f3fde5e2d0e30168dfb7d7e545b1148c6
Joseph Bellahcen <tornupnegatives@gmail.com>
2020-07-16 23:08:16 -0500

    Successfully unpacks first reflector

--- SwiftWizard/Backend/BitPacker/BitPacker.swift
+++ SwiftWizard/Backend/BitPacker/BitPacker.swift
@@ -1,6 +1,36 @@
-//
+// The BitPacker parses FrameData into a hex bitsream
+// During packing, it first reads in that binary values of passed KParameters
+// These values convereted to hex (with an optional "0x" prefix for the Arudino
+// These hex nibbles are reversed, switched, and grouped into a format accetable to the TMS5220
 
 import Foundation
+
+// Used to quickly convert the Double parameters to Int
+extension Dictionary where Value == Double, Key == String {
+    /// Given [String: Double], creates  [String: Int]
+    func doubleValuesToInt() -> [String: Int] {
+        let keys: [String] = Array(self.keys)
+        
+        var intValues: [Int] = []
+        for value: Double in self.values {
+            intValues.append(Int(value))
+        }
+        
+        var newDictionary: [String: Int] = [:]
+        var idx: Int = 0
+        
+        while idx < keys.count {
+            let key: String = keys[idx]
+            let value: Int = intValues[idx]
+            
+            newDictionary[key] = value
+            
+            idx += 1
+        }
+        
+        return newDictionary
+    }
+}
 
 final class BitPacker {
     private static let kByteStreamDelimiter: String = ","
@@ -25,19 +55,11 @@ final class BitPacker {
     
     static func pack(frameData: [FrameData]) -> String {
         var parameterListWithInt: [[String: Int]] = []
-        var keys: [String]
-        var intValues: [Int]
+
         for frame in frameData {
             let dict: [String: Double] = frame.getParameters()
             
-            keys = Array(dict.keys)
-            let values: Dictionary.Values = dict.values
-            
-            intValues = []
-            for value in values {
-                intValues.append(Int(value))
-            }
-            parameterListWithInt.append(Dictionary(uniqueKeysWithValues: zip(keys, intValues)))
+            parameterListWithInt.append(dict.doubleValuesToInt())
         }
         
         
@@ -60,40 +82,48 @@ final class BitPacker {
     }
     
     static func frameDataFor(binary: String) -> [FrameData] {
-        var frames:         [FrameData]    = [FrameData]()
-        let bits:           [Int]          = CodingTable.bits
-        var binaryString:   String?        = binary
+        var frames: [FrameData] = []
+        var binaryString: String? = binary
         
         while binaryString != nil {
-            var frameKeys: [String] = [String]()
+            let frameKeys: [String] = []
             let frame: FrameData = FrameData.frameForDecoding()
             
-            for parameter in CodingTable.parameters {
-                let index: Int = CodingTable.parameters.firstIndex(of: parameter)!
+            for parameter: String in CodingTable.parameters {
+                let index: Int = CodingTable.parameters.firstIndex(of: parameter)! // Exists
                 
-                let parameterBits: Int = bits[index]
-                let length: Int = binaryString!.count
-                let shift: Int = (length < parameterBits) ? (parameterBits - length) : 0
-                let value: Int = BitHelpers.binaryToValue(binary: String(binaryString!.prefix(parameterBits - shift))) << shift
-                
-                binaryString = (length >= parameterBits) ? String(binaryString!.suffix(parameterBits)) : nil
-                
-                frame.setParameter(parameter: parameter, value: Double(value))
-                frameKeys.append(parameter)
-                
-                let parameters: [String: Double] = frame.getParameters()
-                if parametersAreSilenceAndComplete(parameters: parameters) || parametersAreStopAndComplete(parameters: parameters) ||
-                    parametersAreUnvoicedAndComplete(parameters: parameters, frameKeys: frameKeys) ||
-                    parametersAreRepeatedAndComplete(parameters: parameters, frameKeys: frameKeys) {
+                let parameterBits: Int = CodingTable.bits[index]
+                guard let length: Int = binaryString?.count else {
                     break
                 }
                 
+                let shift: Int = (length < parameterBits) ? (parameterBits - length) : 0
+                
+                var substringIndex: String.Index = binaryString!.index(binaryString!.startIndex, offsetBy: parameterBits - shift)
+                let value: Int = BitHelpers.binaryToValue(binary: String(binaryString!.prefix(upTo: substringIndex)))
+
+                if parameterBits > length {
+                    binaryString = nil
+                    break
+                }
+                substringIndex = binaryString!.index(binaryString!.startIndex, offsetBy: parameterBits)
+                binaryString = String(binaryString!.suffix(from: substringIndex))
+                
+                frame.setParameter(parameter: parameter, value: Double(value))
+                frames.append(frame)
+                
+                let parameters: [String: Double] = frame.getParameters()
+                if parametersAreSilenceAndComplete(parameters: parameters) || parametersAreStopAndComplete(parameters: parameters) ||
+                   parametersAreUnvoicedAndComplete(parameters: parameters, frameKeys: frameKeys) ||
+                    parametersAreRepeatedAndComplete(parameters: parameters, frameKeys: frameKeys) || binaryString == nil {
+                    break
+                }
             }
             frames.append(frame)
         }
         return frames
     }
-    
+
     private static func getKeyIndexPairs(key: String, source: [String]) -> [String: Int] {
         var keyIndexPairs: [String: Int] = [String: Int]()
         for index in source.indices {
